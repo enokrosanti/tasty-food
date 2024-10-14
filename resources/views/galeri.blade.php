cf9d0b35184d992a1deddd150ef83012751d0bd1
enokrosanti <enokrosanti54@gmail.com>
2024-10-14 12:28:28 +0700

    feat: make footer responsive

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -59,52 +59,69 @@
     </section>
 
     {{-- Footer --}}
-    <section class="w-full flex flex-col md:flex-row justify-center items-center md:gap-24 py-16 md:py-20 px-4 md:px-14 bg-[#232323]">
-        <div class="text-white flex flex-col gap-4 w-full md:w-1/2">
-            <h2 class="font-bold text-2xl md:text-3xl">Tasty Food</h2>
-            <p class="font-light text-sm md:text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum eaque eveniet saepe! Accusantium sint laudantium, autem similique doloremque reiciendis. Provident ut maiores nulla et vero minus esse dignissimos facere obcaecati, dolores suscipit dolorum tempore voluptatibus saepe. Architecto, soluta aliquam modi officiis dolorum deserunt dolores delectus mollitia, eaque, adipisci omnis! Et?</p>
-            <div class="flex gap-4">
-                <a href="facebook.com"><img src="/img/icons/facebook-icon.png" alt="facebook-icon" class="w-6 h-6 md:w-8 md:h-8"></a>
-                <a href="x.com"><img src="img/icons/twitter-icon.png" alt="twitter-icon" class="w-6 h-6 md:w-8 md:h-8"></a>
+    <footer class="w-full bg-[#232323] text-white py-8 px-4 md:py-16 md:px-14">
+        <section class="flex flex-col md:flex-row justify-center items-start gap-4 md:gap-12">
+            {{-- Tasty Food --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
+                <h2 class="font-bold text-2xl md:text-3xl">Tasty Food</h2>
+                <p class="font-light text-sm md:text-base">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum eaque eveniet saepe! Accusantium sint laudantium, autem similique doloremque reiciendis.
+                </p>
+                <div class="flex gap-4">
+                    <a href="https://facebook.com">
+                        <img src="{{ asset('img/icons/facebook-icon.png') }}" alt="facebook-icon" class="w-6 h-6 md:w-8 md:h-8">
+                    </a>
+                    <a href="https://x.com">
+                        <img src="{{ asset('img/icons/twitter-icon.png') }}" alt="twitter-icon" class="w-6 h-6 md:w-8 md:h-8">
+                    </a>
+                </div>
             </div>
-        </div>
-        <div class="flex flex-col md:flex-row gap-12 w-full md:w-1/2">
-            <div class="text-white flex flex-col gap-4">
-                <h3 class="font-bold text-2xl">Usefull links</h3>
+            
+            {{-- Useful Links --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
+                <h3 class="font-bold text-2xl">Useful Links</h3>
                 <ul class="flex flex-col gap-2 text-base font-normal">
-                    <li><a href="/blog">Blog</a></li>
-                    <li><a href="/hewan">Hewan</a></li>
-                    <li><a href="/galeri">Galeri</a></li>
-                    <li><a href="/testimonial">Testimonial</a></li>
+                    <li><a href="{{ url('/blog') }}" class="hover:underline">Blog</a></li>
+                    <li><a href="{{ url('/hewan') }}" class="hover:underline">Hewan</a></li>
+                    <li><a href="{{ url('/galeri') }}" class="hover:underline">Galeri</a></li>
+                    <li><a href="{{ url('/testimonial') }}" class="hover:underline">Testimonial</a></li>
                 </ul>
             </div>
-            <div class="text-white flex flex-col gap-4">
+            
+            {{-- Privacy --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
                 <h3 class="font-bold text-2xl">Privacy</h3>
                 <ul class="flex flex-col gap-2 text-base font-normal">
-                    <li><a href="/karir">Karir</a></li>
-                    <li><a href="/tentang-kami">Tentang Kami</a></li>
-                    <li><a href="/kontak-kami">Kontak Kami</a></li>
-                    <li><a href="/servis">Servis</a></li>
+                    <li><a href="{{ url('/karir') }}" class="hover:underline">Karir</a></li>
+                    <li><a href="{{ url('/tentang') }}" class="hover:underline">Tentang Kami</a></li>
+                    <li><a href="{{ url('/kontak') }}" class="hover:underline">Kontak Kami</a></li>
+                    <li><a href="{{ url('/servis') }}" class="hover:underline">Servis</a></li>
                 </ul>
             </div>
-            <div class="text-white flex flex-col gap-4">
+            
+            {{-- Contact Info --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
                 <h3 class="font-bold text-2xl">Contact Info</h3>
                 <ul class="flex flex-col gap-2 text-base font-normal">
                     <li class="flex items-center gap-2">
-                        <img src="/img/icons/email-icon.png" alt="email-icon">
-                        <a href="mailto:tastyfood@gmail.com">tastyfood@gmail.com</a>
+                        <img src="{{ asset('img/icons/email-icon.png') }}" alt="email-icon" class="w-4 h-4">
+                        <a href="mailto:tastyfood@gmail.com" class="hover:underline">tastyfood@gmail.com</a>
                     </li>
                     <li class="flex items-center gap-2">
-                        <img src="/img/icons/phone-icon.png" alt="email-icon">
-                        +62 812 3456 7890
+                        <img src="{{ asset('img/icons/phone-icon.png') }}" alt="phone-icon" class="w-4 h-4">
+                        <a href="tel:+6283124568970" class="hover:underline">+62 831 2456 8970</a>
                     </li>
                     <li class="flex items-center gap-2">
-                        <img src="/img/icons/address-icon.png" alt="email-icon">
-                        Kota Bandung, Jawa Barat
+                        <img src="{{ asset('img/icons/address-icon.png') }}" alt="location-icon" class="w-4 h-4">
+                        <a href="https://goo.gl/maps/examplelocation" class="hover:underline">Majalengka, Jawa Barat, Indonesia</a>
                     </li>
                 </ul>
             </div>
-        </div>
-    </section>
+        </section>
+    </footer>
+    
+    <footer class="flex justify-center items-center gap-2 py-4 bg-[#181818]">
+        <p class="text-gray-600 font-normal text-base">Copyright ©2023 All rights reserved</p>
+    </footer>
     </body>
 </html>
--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -78,51 +78,66 @@
     
 
     {{-- Footer --}}
-    <section class="w-full flex flex-col md:flex-row justify-center items-center md:gap-24 py-16 md:py-20 px-4 md:px-14 bg-[#232323]">
-        <div class="text-white flex flex-col gap-4 w-full md:w-1/2 lg:w-1/4">
-            <h2 class="font-bold text-2xl md:text-3xl">Tasty Food</h2>
-            <p class="font-light text-sm md:text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum eaque eveniet saepe! Accusantium sint laudantium, autem similique doloremque reiciendis. Provident ut maiores nulla et vero minus esse dignissimos facere obcaecati, dolores suscipit dolorum tempore voluptatibus saepe. Architecto, soluta aliquam modi officiis dolorum deserunt dolores delectus mollitia, eaque, adipisci omnis! Et?</p>
-            <div class="flex gap-4">
-                <a href="https://facebook.com"><img src="{{ asset('img/icons/facebook-icon.png') }}" alt="facebook-icon" class="w-6 h-6 md:w-8 md:h-8"></a>
-                <a href="https://x.com"><img src="{{ asset('img/icons/twitter-icon.png') }}" alt="twitter-icon" class="w-6 h-6 md:w-8 md:h-8"></a>
+    <footer class="w-full bg-[#232323] text-white py-8 px-4 md:py-16 md:px-14">
+        <section class="flex flex-col md:flex-row justify-center items-start gap-4 md:gap-12">
+            {{-- Tasty Food --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
+                <h2 class="font-bold text-2xl md:text-3xl">Tasty Food</h2>
+                <p class="font-light text-sm md:text-base">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum eaque eveniet saepe! Accusantium sint laudantium, autem similique doloremque reiciendis.
+                </p>
+                <div class="flex gap-4">
+                    <a href="https://facebook.com">
+                        <img src="{{ asset('img/icons/facebook-icon.png') }}" alt="facebook-icon" class="w-6 h-6 md:w-8 md:h-8">
+                    </a>
+                    <a href="https://x.com">
+                        <img src="{{ asset('img/icons/twitter-icon.png') }}" alt="twitter-icon" class="w-6 h-6 md:w-8 md:h-8">
+                    </a>
+                </div>
             </div>
-        </div>
-        <div class="text-white flex flex-col gap-4 w-full md:w-1/2 lg:w-1/4">
-            <h3 class="font-bold text-2xl">Useful Links</h3>
-            <ul class="flex flex-col gap-2 text-base font-normal">
-                <li><a href="{{ url('/blog') }}">Blog</a></li>
-                <li><a href="{{ url('/hewan') }}">Hewan</a></li>
-                <li><a href="{{ url('/galeri') }}">Galeri</a></li>
-                <li><a href="{{ url('/testimonial') }}">Testimonial</a></li>
-            </ul>
-        </div>
-        <div class="text-white flex flex-col gap-4 w-full md:w-1/2 lg:w-1/4">
-            <h3 class="font-bold text-2xl">Privacy</h3>
-            <ul class="flex flex-col gap-2 text-base font-normal">
-                <li><a href="{{ url('/karir') }}">Karir</a></li>
-                <li><a href="{{ url('/tentang-kami') }}">Tentang Kami</a></li>
-                <li><a href="{{ url('/kontak-kami') }}">Kontak Kami</a></li>
-                <li><a href="{{ url('/servis') }}">Servis</a></li>
-            </ul>
-        </div>
-        <div class="text-white flex flex-col gap-4 w-full md:w-1/2 lg:w-1/4">
-            <h3 class="font-bold text-2xl">Contact Info</h3>
-            <ul class="flex flex-col gap-2 text-base font-normal">
-                <li class="flex items-center gap-2">
-                    <img src="{{ asset('img/icons/email-icon.png') }}" alt="email-icon">
-                    <a href="mailto:tastyfood@gmail.com">tastyfood@gmail.com</a>
-                </li>
-                <li class="flex items-center gap-2">
-                    <img src="{{ asset('img/icons/phone-icon.png') }}" alt="phone-icon">
-                    <a href="tel:+6283124568970">+62 831 2456 8970</a>
-                </li>
-                <li class="flex items-center gap-2">
-                    <img src="{{ asset('img/icons/address-icon.png') }}" alt="location-icon">
-                    <a href="https://goo.gl/maps/examplelocation">Majalengka, Jawa Barat, Indonesia</a>
-                </li>
-            </ul>
-        </div>
-    </section>
+            
+            {{-- Useful Links --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
+                <h3 class="font-bold text-2xl">Useful Links</h3>
+                <ul class="flex flex-col gap-2 text-base font-normal">
+                    <li><a href="{{ url('/blog') }}" class="hover:underline">Blog</a></li>
+                    <li><a href="{{ url('/hewan') }}" class="hover:underline">Hewan</a></li>
+                    <li><a href="{{ url('/galeri') }}" class="hover:underline">Galeri</a></li>
+                    <li><a href="{{ url('/testimonial') }}" class="hover:underline">Testimonial</a></li>
+                </ul>
+            </div>
+            
+            {{-- Privacy --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
+                <h3 class="font-bold text-2xl">Privacy</h3>
+                <ul class="flex flex-col gap-2 text-base font-normal">
+                    <li><a href="{{ url('/karir') }}" class="hover:underline">Karir</a></li>
+                    <li><a href="{{ url('/tentang') }}" class="hover:underline">Tentang Kami</a></li>
+                    <li><a href="{{ url('/kontak') }}" class="hover:underline">Kontak Kami</a></li>
+                    <li><a href="{{ url('/servis') }}" class="hover:underline">Servis</a></li>
+                </ul>
+            </div>
+            
+            {{-- Contact Info --}}
+            <div class="flex flex-col gap-4 w-full md:w-1/3">
+                <h3 class="font-bold text-2xl">Contact Info</h3>
+                <ul class="flex flex-col gap-2 text-base font-normal">
+                    <li class="flex items-center gap-2">
+                        <img src="{{ asset('img/icons/email-icon.png') }}" alt="email-icon" class="w-4 h-4">
+                        <a href="mailto:tastyfood@gmail.com" class="hover:underline">tastyfood@gmail.com</a>
+                    </li>
+                    <li class="flex items-center gap-2">
+                        <img src="{{ asset('img/icons/phone-icon.png') }}" alt="phone-icon" class="w-4 h-4">
+                        <a href="tel:+6283124568970" class="hover:underline">+62 831 2456 8970</a>
+                    </li>
+                    <li class="flex items-center gap-2">
+                        <img src="{{ asset('img/icons/address-icon.png') }}" alt="location-icon" class="w-4 h-4">
+                        <a href="https://goo.gl/maps/examplelocation" class="hover:underline">Majalengka, Jawa Barat, Indonesia</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </footer>
     
     <footer class="flex justify-center items-center gap-2 py-4 bg-[#181818]">
         <p class="text-gray-600 font-normal text-base">Copyright ©2023 All rights reserved</p>
